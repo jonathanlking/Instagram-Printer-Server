194ead437f956769ad03cc691394e2168371f831
Jonathan King <jonathanlking@me.com>
2013-07-17 19:42:03 +0100

    Update

--- tools/location.php
+++ tools/location.php
@@ -7,6 +7,7 @@ $latitude = "51.60071198679392";
 $longitude = "-0.1884612590074";
 $token = "1590313.f59def8.ac99b6b1177342a0b64fa9ffc5f736dd";
 
+/*
 //set POST variables
 
 $fields = array(
@@ -36,6 +37,19 @@ $url = $url.$$fields_string;
 echo($url);
 
 var_dump($data);
+*/
 
+
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => "https://api.instagram.com/v1/locations/search?lat=$latitude&lng=$longitude&access_token=$token",
+));
+
+$responce = curl_exec($curl);
+curl_close($curl);
+
+var_dump($responce);
 
 ?>
