4bdd4808ac3291263f87611ed12f1e5de488e293
Jonathan King <jonathanlking@me.com>
2013-07-19 16:34:14 +0100

    Update instagram.php

--- tools/location/instagram.php
+++ tools/location/instagram.php
@@ -1,20 +1,5 @@
 <?php
 
-/*
-$url = "https://api.instagram.com/v1/locations/search";
-
-$latitude = $_GET["lat"];
-$longitude = $_GET["lng"];
-$token = "1590313.22c41e6.ab449188c6c64d068c84904b7709255f";
-
-$curl = curl_init();
-// Set some options - we are passing in a useragent too here
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => "https://api.instagram.com/v1/locations/search?lat=$latitude&lng=$longitude&access_token=$token",
-));
-*/
-
 $foursquareID = $_GET["id"];
 $clientID = "cd5ca076bfd6494ba94756a919325d34";
 
@@ -30,7 +15,5 @@ $responce = curl_exec($curl);
 curl_close($curl);
 
 echo $responce;
-
-/* echo file_get_contents("sample.txt"); */
 
 ?>
