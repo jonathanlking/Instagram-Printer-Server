86545550e6092e25c1c625ec800b4e4f26ccb78a
Jonathan King <jonathanlking@me.com>
2013-07-19 17:19:38 +0100

    Update keychain.php

--- keychain.php
+++ keychain.php
@@ -1,5 +1,25 @@
 <?php
 
+/* Import using: include($_SERVER['DOCUMENT_ROOT']."/keychain.php"); */
 
+function foursquare()
+{
+  $token = "KFHT0LQTJQU1W41QSMC3QBUZGDGRYWMSFE5PIJSK14K35R4W&v=20130718";
+	return $token;
+
+}
+
+function instagram()
+{
+
+	$credentials = array(
+		"id" => "cd5ca076bfd6494ba94756a919325d34",
+		"secret" => "93b2abd197d244e2b5adaab20263d0aa",
+		"url" => "http://instagram.jonathanlking.com",
+		"uri" => "http://instagram.jonathanlking.com/services/oauth"
+	);
+
+	return $credentials;
+}
 
 ?>
