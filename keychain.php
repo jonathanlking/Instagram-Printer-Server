b0d067845576940867c60f6fe1b3eb19ccd2a661
Jonathan King <jonathanlking@me.com>
2013-07-20 12:11:35 +0100

    Update keychain.php

--- keychain.php
+++ keychain.php
@@ -23,24 +23,4 @@ class keychain
     }  
 }  
 
-function foursquare()
-{
-  $token = "KFHT0LQTJQU1W41QSMC3QBUZGDGRYWMSFE5PIJSK14K35R4W&v=20130718";
-	return $token;
-
-}
-
-function instagram()
-{
-
-	$credentials = array(
-		"id" => "cd5ca076bfd6494ba94756a919325d34",
-		"secret" => "93b2abd197d244e2b5adaab20263d0aa",
-		"url" => "http://instagram.jonathanlking.com",
-		"uri" => "http://instagram.jonathanlking.com/services/oauth"
-	);
-
-	return $credentials;
-}
-
 ?>
